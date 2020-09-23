74e05d5a6379b420f21ead19b2446fe7115a1697
Vinicius Cruz <vinicius.cruza@gmail.com>
2020-09-22 22:51:24 -0300

    initial commit + database connection

---
+++ src/models/Appointment.ts
@@ -0,0 +1,18 @@
+import { uuid } from "uuidv4";
+import { v4 } from 'uuid';
+
+class Appointment {
+    id: string;
+
+    provider: string;
+
+    date: Date;
+
+    constructor({ provider, date }: Omit<Appointment, 'id'>) {
+        this.provider = provider;
+        this.date = date;
+        this.id = v4();
+    }
+}
+
+export default Appointment;
